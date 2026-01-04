9fa33e11b1d327421318ff6db2e50ee2ff67c9f7
emilymontec <emilymonterrosa.c@gmail.com>
2026-01-03 23:24:56 -0500

    Actualizacion app; ruta /about

--- BasicWebApp/BasicWebApp/urls.py
+++ BasicWebApp/BasicWebApp/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home
+from app.views import home, about
 
 urlpatterns = [
-    path('', home, name='home'),
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('about/', about, name='about'),
 ]
